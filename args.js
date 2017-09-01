5364f5075db82ed5ca6197365060f53713afb23e
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-09-01 10:51:45 -0500

    arggggggggg

---
+++ args.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// argv holds whatever we typed in the terminal to launch the application, like "node main.js hello"
+console.log('the arguments', process.argv );
+let myArg = process.argv[2];
+console.log(`Hello, ${myArg}`);
+
+// capture multiple args, no matter how many, using destructuring assignment
+let [,, ...myArgs] = process.argv;
